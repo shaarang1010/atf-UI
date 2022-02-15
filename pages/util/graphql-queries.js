a918c5a75e128096ea911e126e45078f60dc2c81
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-02-15 22:54:38 +1100

    add graphql queries

---
+++ pages/util/graphql-queries.js
@@ -0,0 +1,70 @@
+import { gql } from "@apollo/client";
+
+const authenticateUser = (email, password) => {
+  const loginQuery = gql`
+    mutation {
+        login(input: { identifier: "${email}", password: "${password}" }) {
+          jwt
+        }
+      }
+    `;
+  return loginQuery;
+};
+
+const getTherapyDetailsById = (id) => {
+  const therapyQuery = gql`
+    therapyProfile(id: ${id}){
+        created_at
+        updated_at
+        therapyname
+        summaryStatement
+        relatedTherapies
+        levelOfEvidence{
+            evidenceStatement
+            additionalText
+        }
+        therapyTargets{
+            icfDomains
+            therapeuticTargets
+            clientSelection{
+                aphasiaText
+                aphasiaSeverity
+                aphasiaAetiology
+                timeSinceOnsetOfAphasia
+            }
+        }
+        therapyIngredients{
+            therapyProtocol
+            keyTherapeuticPrincipals
+            therapyMode{
+                setting
+                groupSize
+                doseAndSchedule
+                teleHealth
+            }
+            therapyMethod
+            resources
+        }
+        mechainismOfAction{
+            theoreticalUnderPinnings
+            supportingEmpiricalEvidence
+        }
+        therapyResources{
+            videoFile{
+            name
+            alternativeText
+            previewUrl
+            url
+            caption
+            }
+            literature
+            other
+        }
+        alternativeNames
+        published_at
+    }
+`;
+  return therapyQuery;
+};
+
+export { authenticateUser, getTherapyDetailsById };
