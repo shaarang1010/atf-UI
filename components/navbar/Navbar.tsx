e90d4400778ea33e3663e82022903b7a1ca0ae73
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-02-17 23:17:41 +1100

    add default theme and add Navbar component

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,23 +1,41 @@
-import { Box, Flex, Spacer } from "@chakra-ui/react";
+import { Box, Button, Flex, Spacer } from "@chakra-ui/react";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 
 type NavOptions = {
-    optionName: string;
-    optionLink: string;
-    optionColor: string;
-}
+  optionName: string;
+  optionLink: string;
+  optionColor: string;
+};
 type NavbarProps = {
-    navOptions: NavOptions[];
-    navColor: string;
-}
+  navOptions: NavOptions[];
+  navColor: string;
+};
 
+const NavBar: NextPage<NavbarProps> = ({ navOptions, navColor }) => {
+  const router = useRouter();
+  const onClickNavigate = (e, link) => {
+    e.preventDefault();
+    router.push(link);
+  };
+  return (
+    <Box width='100%' p={6} bg={navColor} h='20' overflow='hidden'>
+      <Flex>
+        <Spacer />
+        {navOptions.map((option, index) => {
+          <Box key={index}>
+            <Button
+              colorScheme={option.optionColor}
+              variant='outline'
+              onClick={(e) => onClickNavigate(e, option.optionLink)}
+            >
+              {option.optionName}
+            </Button>
+          </Box>;
+        })}
+      </Flex>
+    </Box>
+  );
+};
 
-const NavBar: NextPage<NavbarProps> = ({navOptions, navColor}) =>{
-    return(
-        <Box width="100%" p={6} bg={navColor} h="20" overflow="hidden">
-            <Flex>
-                <Spacer/>
-            </Flex>
-        </Box>
-    )
-}
+export default NavBar;
---
+++ styles/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    transparent: "transparent",
+    black: "#000",
+    white: "#fff",
+    gray: {
+      50: "#f7fafc",
+      900: "#171923"
+    },
+    darkBlue: "#2A4365"
+  }
+};
+
+export default theme;
