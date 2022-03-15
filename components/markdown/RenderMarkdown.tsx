08c99fddf4edd97e16fc8109ddb31df473099fb8
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-03-15 22:52:44 +1100

    change markdown lib and add appollo provider

--- components/markdown/RenderMarkdown.tsx
+++ components/markdown/RenderMarkdown.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { formatMarkdownText } from "../../util/format-markdown-text";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeReact from "rehype-react";
 
 interface MarkdownProps {
   markdownText: string;
@@ -9,7 +13,14 @@ interface MarkdownProps {
 
 const RenderMarkdownToHTML: React.FC<MarkdownProps> = ({ markdownText }) => {
   const formattedMarkdownText = formatMarkdownText(markdownText, { from: "NEWLINE", to: "\n \n " });
-  return <ReactMarkdown remarkPlugins={[remarkGfm]} children={formattedMarkdownText}></ReactMarkdown>;
+  const content = unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeReact, { createElement: React.createElement })
+    .processSync(formattedMarkdownText).result;
+
+  // return <ReactMarkdown remarkPlugins={[remarkGfm]} children={formattedMarkdownText}></ReactMarkdown>;
+  return <>{content}</>;
 };
 
 export default RenderMarkdownToHTML;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,21 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/Layout";
 import { ChakraProvider } from "@chakra-ui/provider";
+import { ApolloProvider } from "@apollo/client";
 import theme from "../styles/theme";
+import client from "../util/apollo-client";
 import { Fonts } from "../styles/Fonts";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Fonts />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <Fonts />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
