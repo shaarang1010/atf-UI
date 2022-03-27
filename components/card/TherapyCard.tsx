215e0790849e098cfba87e2d13920437d5ae48ec
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-03-27 21:02:23 +1100

    add therapy card and svg assets

--- components/card/TherapyCard.tsx
+++ components/card/TherapyCard.tsx
@@ -1,20 +1,52 @@
 import React from "react";
-import {
-  Box,
-  Grid,
-  GridItem,
-  Button,
-  FormControl,
-  Heading,
-  FormLabel,
-  Input,
-  Text,
-  Link,
-  Icon
-} from "@chakra-ui/react";
+import { Box, Heading, FormLabel, Input, Text, Link, Icon, Badge, SimpleGrid } from "@chakra-ui/react";
+import theme from "../../styles/theme";
+import { BsFillCameraVideoFill } from "react-icons/bs";
 
 interface TherapyCardProps {
   cardTitle: string;
   videoSource: string;
   therapyTargets: React.ReactNode;
+  summaryStatement: string;
+  levelOfEvidence: string;
 }
+
+const TherapyCard: React.FC<TherapyCardProps> = ({
+  cardTitle,
+  videoSource,
+  therapyTargets,
+  summaryStatement,
+  levelOfEvidence
+}) => {
+  return (
+    <Box maxW='md' borderWidth='1px' borderRadius='lg' overflow='hidden' p='3'>
+      <Box display={"flex"} alignItems='baseline'>
+        <Link>
+          <Heading as='h3' size='lg'>
+            {cardTitle}
+          </Heading>
+        </Link>
+        <Box bg={theme.colors.gray.default} width={10} ml='5' borderRadius='md'>
+          <Icon as={BsFillCameraVideoFill} fill='blue' ml='3' mt='3' />
+        </Box>
+      </Box>
+      <Box mt='5'>
+        <Badge borderRadius='full' px='2' colorScheme='gray' textTransform='uppercase' fontSize='0.8em'>
+          Targets
+        </Badge>
+        <Box mt='3'>{therapyTargets}</Box>
+      </Box>
+      <Box mt='5'>
+        <Text>{summaryStatement}</Text>{" "}
+      </Box>
+      <Box mt='5'>
+        <Badge borderRadius='full' px='2' colorScheme='gray' textTransform='uppercase'>
+          Level of Evidence
+        </Badge>
+        <Box mt='3'>{levelOfEvidence}</Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default TherapyCard;
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
-import { Container, Grid, GridItem, Box, CheckboxGroup, Checkbox, Stack, SimpleGrid } from "@chakra-ui/react";
-import SearchProfile from "../components/forms/searchProfile/searchProfile";
+import React, { useEffect, useState } from "react";
+import { Container, Grid, GridItem, Box, CheckboxGroup, Checkbox, Stack, SimpleGrid, Image } from "@chakra-ui/react";
+import SearchProfile from "../components/forms/searchProfile/SearchProfile";
+import Speaking from "../assets/speaking.svg";
+import Writing from "../assets/writing.svg";
+import TherapyCard from "../components/card/TherapyCard";
 
 const filterValues = [
   {
@@ -37,6 +40,10 @@ const TherapySearch = () => {
     </CheckboxGroup>
   );
 
+  useEffect(() => {
+    console.log(Speaking);
+  }, []);
+
   return (
     <Container maxW={"container.lg"}>
       <SimpleGrid columns={{ sm: 2, md: 2 }} gap={5}>
@@ -49,7 +56,19 @@ const TherapySearch = () => {
           />
         </Box>
         <Box maxW={"lg"} mt='20'>
-          <h1>Text</h1>
+          <TherapyCard
+            cardTitle='Test Therapy'
+            summaryStatement='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae nibh mauris. Proin at ante tortor. Donec lacus diam, convallis et laoreet ac, varius sed ante. Donec condimentum tempor aliquam. Vestibulum vitae efficitur velit, nec convallis nibh. Etiam nec ipsum nibh. Vestibulum ac blandit ligula, auctor molestie ipsum. Sed porttitor ullamcorper lacus nec imperdiet. Donec nisl urna, efficitur in felis non, mollis mollis erat. Nam ligula odio, aliquet varius lectus eget, faucibus lacinia erat. Nulla enim justo, placerat non mauris quis, pretium eleifend magna. Vivamus at velit mi. Curabitur tincidunt sed justo sit amet varius.
+          # Nunc ullamcorper, leo a consectetur vestibulum,'
+            therapyTargets={
+              <>
+                <Image src={Speaking.src} alt='Speaking' />
+              </>
+            }
+            levelOfEvidence='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae nibh mauris. Proin at ante tortor. Donec lacus diam, convallis et laoreet ac, varius sed ante. Donec condimentum tempor aliquam. Vestibulum vitae efficitur velit, nec convallis nibh. Etiam nec ipsum nibh. Vestibulum ac blandit ligula, auctor molestie ipsum. Sed porttitor ullamcorper lacus nec imperdiet. Donec nisl urna, efficitur in felis non, mollis mollis erat. Nam ligula odio, aliquet varius lectus eget, faucibus lacinia erat. Nulla enim justo, placerat non mauris quis, pretium eleifend magna. Vivamus at velit mi. Curabitur tincidunt sed justo sit amet varius.
+          # Nunc ullamcorper, leo a consectetur vestibulum,'
+            videoSource='abcd'
+          />
         </Box>
       </SimpleGrid>
     </Container>
